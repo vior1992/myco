7a2c190cf3150bc0df5f1bde49ebba2cb0d117b7
Dani <vior1992@gmail.com>
2020-09-11 09:53:54 +0300

    updated validation messages & added model tests

--- api/app/models/contact.rb
+++ api/app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-    validates :first_name, :last_name, :email, :phone_number, presence: { message: "%{attribute} can't be empty or blank" }
+    validates :first_name, :last_name, :email, :phone_number, presence: { message: "%{attribute} can not be empty or blank" }
     validates :email, format: { with: /\A(\S+)@(.+)\.(\S+)\z/, message: "Oops! %{attribute} address must contain @ and domain e.g. name@domain.com" }
-    validates :phone_number, format: { with: /\A[+\d](?:.*\d)?\z/, message: "%{attribute} just allows numbers and '+' sign" }
+    validates :phone_number, format: { with: /\A[+\d](?:.*\d)?\z/, message: "%{attribute} only allows numbers and '+' sign" }
 end
--- api/test/models/contact_test.rb
+++ api/test/models/contact_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save new contact" do
+    contact = Contact.new(first_name: 'test', last_name: 'model', email: 'test@model.com', phone_number: '00 00 00')
+
+    assert contact.save
+  end
+
+  test "should not save contact without required values" do
+    contact = Contact.new
+
+    assert_not contact.save
+  end
+
+  test "should not save contact when first_name value is empty or blank" do
+    contact = Contact.new(first_name: '', last_name: 'model', email: 'test@model.com', phone_number: '00 00 00')
+
+    assert_not contact.save
+  end
+
+  test "should not save contact when last_name value is empty or blank" do
+    contact = Contact.new(first_name: 'test', last_name: '', email: 'test@model.com', phone_number: '00 00 00')
+
+    assert_not contact.save
+  end
+
+  test "should not save contact when email value is empty or blank" do
+    contact = Contact.new(first_name: 'test', last_name: 'model', email: '', phone_number: '00 00 00')
+
+    assert_not contact.save
+  end
+
+  test "should not save contact when email value not match format validation" do
+    contact = Contact.new(first_name: 'test', last_name: 'model', email: 'email.email', phone_number: '00 00 00')
+
+    assert_not contact.save
+  end
+
+  test "should not save contact when phone_number value is empty or blank" do
+    contact = Contact.new(first_name: 'test', last_name: 'model', email: 'test@model.com', phone_number: '')
+
+    assert_not contact.save
+  end
+
+  test "should not save contact when phone_number value not match format validation" do
+    contact = Contact.new(first_name: 'test', last_name: 'model', email: 'test@model.com', phone_number: 'number 00')
+
+    assert_not contact.save
+  end
 end
