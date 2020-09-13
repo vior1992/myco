b6d18ad9d8105ee6d5ae13cb89b224536e30e9e7
Dani <vior1992@gmail.com>
2020-09-13 16:30:47 +0300

    add redirect

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import ContactsFactory from './components/ContactsFactory';
 import ContactPage from './components/ContactPage';
 
@@ -15,6 +20,7 @@ const Routes = () => (
       <Route path="/contacts/:id">
         <ContactPage />
       </Route>
+      <Redirect to="/" />
     </Switch>
   </Router>
 );
